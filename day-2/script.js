093f8d459ad04bdfb8de78015cda0c8e1b62075b
Melissa Villegas <melissavillegas0898@gmail.com>
2022-07-28 15:52:13 -0400

    organized scipt.js and added circle-center

--- day-2/script.js
+++ day-2/script.js
@@ -1,30 +1,23 @@
-//STEP 4
 const secondHand = document.querySelector(".second-hand");
-//STEP 6 by myself
-const hourHand = document.querySelector(".hour-hand");
 const minHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
-  //STEP 1
-  // console.log("hi");
-  //STEP 2
   const now = new Date();
-  const seconds = now.getSeconds();
-  console.log("seconds", seconds);
-  //STEP 3
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  //STEP 5
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  //STEP 7 by meself
+
+  const hours = now.getHours();
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
   const minutes = now.getMinutes();
-  console.log("minutes", minutes);
   const minutesDegrees = (minutes / 60) * 360 + 90;
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
-  const hours = now.getHours();
-  console.log("hours", hours);
-  const hoursDegrees = (hours / 12) * 360 + 90;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  console.log(`${hours}hrs:${minutes}min:${seconds}sec`);
 }
 
 setInterval(setDate, 1000);
