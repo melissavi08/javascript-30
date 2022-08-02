cac6c46b9b667aa88b66dc3b581a050ba67215df
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-01 21:08:06 -0400

    script.js organized and readme.md added day-6

--- day-6/script.js
+++ day-6/script.js
@@ -3,20 +3,10 @@ const endpoint =
 
 const cities = [];
 
-//STEP-1
-// const prom = fetch(endpoint);
-// console.log(prom);
-
-// STEP-2
 fetch(endpoint)
-  // .then(blob => console.log(blob));
-  // STEP-3
   .then(blob => blob.json())
-  // .then(data => console.log(data));
-  // STEP-4
-  .then(data => cities.push(...data)); // or we can change const for let in cities
+  .then(data => cities.push(...data));
 
-// STEP-5
 function findMatches(wordToMatch, cities) {
   return cities.filter(place => {
     const regex = new RegExp(wordToMatch, "gi");
@@ -24,27 +14,14 @@ function findMatches(wordToMatch, cities) {
   });
 }
 
-//STEP-13
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-// STEP-6
 function displayMatches() {
-  // console.log(this.value);
-  // STEP-8
   const matchArray = findMatches(this.value, cities);
-  // console.log(matchArray);
-  // STEP-9
   const html = matchArray
     .map(place => {
-      //   return `
-      // <li>
-      //   <span class="name">${place.city}, ${place.state}</span>
-      //   <span class="population">${place.population}</span>
-      // </li>`;
-      // })
-      //STEP-11
       const regex = new RegExp(this.value, "gi");
       const cityName = place.city.replace(
         regex,
@@ -54,19 +31,16 @@ function displayMatches() {
         regex,
         `<span class="hl">${this.value}</span>`
       );
-      //STEP-12 (name) and STEP-14 (population)
       return `
     <li>
       <span class="name">${cityName}, ${stateName}</span>
       <span class="population">${numberWithCommas(place.population)}</span>
     </li>`;
     })
-    // STEP-10
     .join("");
   suggestions.innerHTML = html;
 }
 
-// STEP-7
 const searchInput = document.querySelector(".search");
 const suggestions = document.querySelector(".suggestions");
 
