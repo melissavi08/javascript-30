90426124871d974262b75c2dcb9357baf0a48cfb
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-02 18:57:36 -0400

    step by step day-7

--- day-7/script.js
+++ day-7/script.js
@@ -17,12 +17,37 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+// const someOlder = people.some(person => {
+//   const currentYear = new Date().getFullYear();
+//   return currentYear - person.year >= 19;
+// });
+const someOlder = people.some(
+  person => new Date().getFullYear() - person.year >= 19
+);
+console.log({ someOlder });
+
 // Array.prototype.every() // is everyone 19 or older?
+// const everyOlder = people.every(person => {
+//   const currentYear = new Date().getFullYear();
+//   return currentYear - person.year >= 19;
+// });
+const everyOlder = people.every(
+  person => new Date().getFullYear() - person.year >= 19
+);
+
+console.log({ everyOlder });
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const findComment = comments.find(comment => comment.id === 823423);
+console.log({ findComment });
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const commentIndex = comments.findIndex(comment => comment.id === 823423);
+console.log({ commentIndex });
+
+comments.splice(commentIndex, 1);
+console.log({ comments });
