349b52a97a065050e1a21c6293c0d9a2f5b574d7
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-08 17:31:26 -0400

    start files day-13 and .js day-12 organized

--- day-12/script.js
+++ day-12/script.js
@@ -1,17 +1,13 @@
-// STEP 2
 const pressed = [];
 const secretCode = "melissa";
 
-// STEP 1
 window.addEventListener("keyup", e => {
   console.log(e.key);
-  // STEP 3
   pressed.push(e.key);
-  // STEP 4
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
   if (pressed.join("").includes(secretCode)) {
     console.log("DING DING!");
     cornify_add();
   }
-  console.log(pressed); // STEP 3
+  console.log(pressed);
 });
