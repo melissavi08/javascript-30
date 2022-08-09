2cc06235800c5dcbd2ea95e962a63e5391daeacc
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-09 17:46:33 -0400

    day-14 completed

--- day-14/script.js
+++ day-14/script.js
@@ -1,42 +1,103 @@
 // start with strings, numbers and booleans
+let age = 100;
+let age2 = age;
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
+
+let name = "Meli";
+let name2 = name;
+console.log(name, name2);
+name = "Melissa";
+console.log(name, name2);
 
 // Let's say we have an array
 const players = ["Wes", "Sarah", "Ryan", "Poppy"];
 
 // and we want to make a copy of it.
+const team = players;
+console.log(players, team);
 
 // You might think we can just do something like this:
-
+// team[3] = "Lux";
+// console.log(players, team);
 // however what happens when we update that array?
-
 // now here is the problem!
-
 // oh no - we have edited the original array too!
-
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
-
+const team2 = players.slice();
+console.group("Team2");
+console.log("players", players, "team2", team2);
+team2[3] = "Lux";
+console.log("players", players, "team2", team2);
+console.groupEnd("Team2");
 // one way
 
 // or create a new array and concat the old one in
+const team3 = [].concat(players);
+console.group("Team3");
+console.log("players", players, "team3", team3);
+team3[3] = "Lux";
+console.log("players", players, "team3", team3);
+console.groupEnd("Team3");
 
 // or use the new ES6 Spread
+const team4 = [...players];
+console.group("Team4");
+console.log("players", players, "team4", team4);
+team4[3] = "Lux";
+console.log("players", players, "team4", team4);
+console.groupEnd("Team4");
 
+const team5 = Array.from(players);
+console.group("Team5");
+console.log("players", players, "team5", team5);
+team5[3] = "Lux";
+console.log("players", players, "team5", team5);
+console.groupEnd("Team5");
 // now when we update it, the original one isn't changed
 
 // The same thing goes for objects, let's say we have a person object
-
 // with Objects
 const person = {
   name: "Wes Bos",
   age: 80,
 };
+console.log("person", person);
 
 // and think we make a copy:
+// const captain = person;
+// captain.number = 99;
+// console.log("person", person, "captain", captain);
 
 // how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99 });
+console.log("person", person, "cap2", cap2);
 
 // We will hopefully soon see the object ...spread
+const cap3 = { ...person };
+cap3.number = 99;
+console.log("person", person, "cap3", cap3);
 
 // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+const wes = {
+  name: "Wes",
+  age: 100,
+  social: {
+    twitter: "@wesbos",
+    facebook: "wesbos.developer",
+  },
+};
+console.log(wes);
+
+const dev = Object.assign({}, wes);
+dev.name = "Wesley";
+console.log("wes", wes, "dev", dev);
+// dev.social.twitter = "@coolman";
+// console.log("wes.social", wes.social, "dev.social", dev.social);
+
+const dev2 = JSON.parse(JSON.stringify(wes));
+dev2.social.twitter = "@coolman";
+console.log("wes.social", wes.social, "dev2.social", dev2.social);
