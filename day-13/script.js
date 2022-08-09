9af6da82230e28a48490b7ad897d4fc8bbe29257
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-08 20:38:57 -0400

    step by step day-13

--- day-13/script.js
+++ day-13/script.js
@@ -13,3 +13,29 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+// STEP 1
+const sliderImages = document.querySelectorAll(".slide-in");
+
+// STEP 2
+function checkSlide(e) {
+  // console.count(e);
+  // STEP 5
+  // console.log(window.scrollY);
+  sliderImages.forEach(sliderImage => {
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // console.log(slideInAt);
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShow = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.screenY < imageBottom;
+    if (isHalfShow && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+// window.addEventListener("scroll", checkSlide); // STEP 3
+window.addEventListener("scroll", debounce(checkSlide)); // STEP 4
