e4611933f7a394b4c9b7ee44d42825b39ad6d09c
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-10 21:54:23 -0400

    readme.md day-15

---
+++ day-15/Readme.md
@@ -0,0 +1,21 @@
+# JavaScript30 - Day 15
+
+Hi, there:wave:
+
+:woman_technologist: Here is **Day-15 of JavaScript30**, following step by step the tutorial.
+
+:computer: [JavaScript 30](https://javascript30.com/) is a 30 Day Vanilla JS Coding Challenge
+
+:x: No Frameworks / No Compilers / No Libraries
+
+## Screenshot
+
+<img src="./img/screenshot15.jpg" alt="screenshot15" style="width:70%;"/>
+
+## Live site
+
+- [Day-15/30](https://melissavi08.github.io/javascript-30/day-15/index.html)
+
+## Connect with me
+
+<a href='https://twitter.com/melissa_vi2' target="_blank"><img alt='Twitter' src='https://img.shields.io/badge/melissa__vi2-100000?style=flat&logo=Twitter&logoColor=white&labelColor=00BFFF&color=FF69B4'/></a> <a href='https://www.linkedin.com/in/melissa-villegas' target="_blank"><img alt='LinkedIn' src='https://img.shields.io/badge/Melissa_Villegas-100000?style=flat&logo=LinkedIn&logoColor=white&labelColor=00BFFF&color=FF69B4'/></a>
--- day-15/script.js
+++ day-15/script.js
@@ -1,25 +1,20 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = JSON.parse(localStorage.getItem("items")) || []; // STEP 9
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
-//STEP 2
 function addItem(e) {
-  // console.log("Hello");
-  // STEP 3
   e.preventDefault();
   const text = this.querySelector("[name=item]").value;
   const item = {
     text,
     done: false,
   };
-  // console.log(item);
   items.push(item);
-  populateList(items, itemsList); // STEP 5
-  localStorage.setItem("items", JSON.stringify(items)); // STEP 7
+  populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
   this.reset();
 }
 
-// STEP 4 and STEP 6
 function populateList(plates = [], platesList) {
   platesList.innerHTML = plates
     .map((plate, i) => {
@@ -35,9 +30,7 @@ function populateList(plates = [], platesList) {
     .join("");
 }
 
-// STEP 10
 function toggleDone(e) {
-  // STEP 12
   if (!e.target.matches("input")) return;
   const el = e.target;
   const index = el.dataset.index;
@@ -47,7 +40,6 @@ function toggleDone(e) {
   console.log(e.target);
 }
 
-// STEP 1
 addItems.addEventListener("submit", addItem);
-itemsList.addEventListener("click", toggleDone); // STEP 11
-populateList(items, itemsList); // STEP 8
+itemsList.addEventListener("click", toggleDone);
+populateList(items, itemsList);
