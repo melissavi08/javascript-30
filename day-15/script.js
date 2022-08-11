ea7e3a87d449d2f438a8114a6d30615b242b2f6a
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-10 20:19:30 -0400

    step by step day-15 part-1

--- day-15/script.js
+++ day-15/script.js
@@ -1,3 +1,35 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
+
+//STEP 2
+function addItem(e) {
+  // console.log("Hello");
+  // STEP 3
+  e.preventDefault();
+  const text = this.querySelector("[name=item]").value;
+  const item = {
+    text,
+    done: false,
+  };
+  // console.log(item);
+  items.push(item);
+  populateList(items, itemsList); // STEP 5
+  this.reset();
+}
+
+// STEP 4
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+    <li>
+      <label for="">${plate.text}</label>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+//STEP 1
+addItems.addEventListener("submit", addItem);
