202323fa0982d30b5c1ba77093a7c88a6240ee33
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-10 21:47:17 -0400

    step by step day-15 part-2

--- day-15/script.js
+++ day-15/script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
-const items = [];
+const items = JSON.parse(localStorage.getItem("items")) || []; // STEP 9
 
 //STEP 2
 function addItem(e) {
@@ -15,21 +15,39 @@ function addItem(e) {
   // console.log(item);
   items.push(item);
   populateList(items, itemsList); // STEP 5
+  localStorage.setItem("items", JSON.stringify(items)); // STEP 7
   this.reset();
 }
 
-// STEP 4
+// STEP 4 and STEP 6
 function populateList(plates = [], platesList) {
   platesList.innerHTML = plates
     .map((plate, i) => {
       return `
     <li>
-      <label for="">${plate.text}</label>
+      <input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? "checked" : ""
+      }/>
+      <label for="item${i}">${plate.text}</label>
     </li>
     `;
     })
     .join("");
 }
 
-//STEP 1
+// STEP 10
+function toggleDone(e) {
+  // STEP 12
+  if (!e.target.matches("input")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+  console.log(e.target);
+}
+
+// STEP 1
 addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone); // STEP 11
+populateList(items, itemsList); // STEP 8
