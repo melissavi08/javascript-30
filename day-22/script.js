7c5469a8e75a0fc356e2f1371a4e18d992a4ffc9
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-18 20:40:24 -0400

    readme.md day-22

--- day-22/script.js
+++ day-22/script.js
@@ -1,30 +1,22 @@
-// STEP 1
 const triggers = document.querySelectorAll("a");
 const highlight = document.createElement("span");
 highlight.classList.add("highlight");
 document.body.appendChild(highlight);
 
-// STEP 2
 function highlightLink() {
-  // console.log(this);
-  // STEP 4
   const linkCoords = this.getBoundingClientRect();
   console.log(linkCoords);
-  // highlight.style.width = `${linkCoords.width}px`;
-  // highlight.style.height = `${linkCoords.height}px`;
-  // highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
-  // STEP 5
+
   const coords = {
     width: linkCoords.width,
     height: linkCoords.height,
     top: linkCoords.top + window.scrollY,
     left: linkCoords.left + window.scrollX,
   };
-  // STEP 6
+
   highlight.style.width = `${coords.width}px`;
   highlight.style.height = `${coords.height}px`;
   highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
-// STEP 3
 triggers.forEach(a => a.addEventListener("mouseenter", highlightLink));
