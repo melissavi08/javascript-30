bba4e4302a1a37234268dac8b58c285ef395ea5d
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-20 20:47:46 -0400

    readme.md day-24 and day-25

--- day-24/script.js
+++ day-24/script.js
@@ -1,22 +1,14 @@
-// STEP 1
 const nav = document.querySelector("#main");
-// STEP 4
 const topOfNav = nav.offsetTop;
 
-// STEP 2
 function fixNav() {
-  // STEP 5
-  // console.log(topOfNav);
   if (window.scrollY >= topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + "px"; // STEP 7
+    document.body.style.paddingTop = nav.offsetHeight + "px";
     document.body.classList.add("fixed-nav");
   } else {
-    document.body.style.paddingTop = 0; // STEP 8
+    document.body.style.paddingTop = 0;
     document.body.classList.remove("fixed-nav");
   }
 }
 
-// STEP 3
 window.addEventListener("scroll", fixNav);
-
-// STEPS 6, 9 & 10 are in style.css
--- day-25/script.js
+++ day-25/script.js
@@ -1,29 +1,19 @@
-// STEP 1
 const divs = document.querySelectorAll("div");
-// STEP 7
 const button = document.querySelector("button");
 
-// STEP 2
 function logText(e) {
   console.log(this.classList.value);
-  // STEP 5 stop bubbling!!!
+  // stop bubbling!!!
   // e.stopPropagation();
 }
 
-// STEP 3
-// divs.forEach(div => div.addEventListener("click", logText));
-
-// STEP 4
 divs.forEach(div =>
   div.addEventListener("click", logText, {
-    // capture: true,
     capture: false,
-    // STEP 6
     once: true,
   })
 );
 
-// STEP 8
 button.addEventListener(
   "click",
   () => {
