488f1da7495a34aa96aa4ff8b2f220fcd22558e1
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-26 20:58:57 -0400

    html and css changes + js organized

--- day-26/script.js
+++ day-26/script.js
@@ -1,41 +1,28 @@
-// STEP 1
 const triggers = document.querySelectorAll(".cool > li");
 const background = document.querySelector(".dropdownBackground");
 const nav = document.querySelector(".top");
 
-// STEP 2
 function handleEnter() {
-  // STEP 5
-  console.log("ENTER!!!");
   this.classList.add("trigger-enter");
   setTimeout(
     () =>
-      // STEP 11
       this.classList.contains("trigger-enter") &&
       this.classList.add("trigger-enter-active"),
-    // {
-    //   STEP 10 if line
-    //   if (this.classList.contains("trigger-enter")) {
-    //     this.classList.add("trigger-enter-active");
-    //   }
-    // },
     150
   );
-  // STEP 7
   background.classList.add("open");
-  // STEP 9
+
   const dropdown = this.querySelector(".dropdown");
-  console.log(dropdown);
   const dropdownCoords = dropdown.getBoundingClientRect();
-  console.log(dropdownCoords);
   const navCoords = nav.getBoundingClientRect();
-  console.log(navCoords);
+
   const coords = {
     height: dropdownCoords.height,
     width: dropdownCoords.width,
     top: dropdownCoords.top - navCoords.top,
     left: dropdownCoords.left - navCoords.left,
   };
+
   background.style.setProperty("width", `${coords.width}px`);
   background.style.setProperty("height", `${coords.height}px`);
   background.style.setProperty(
@@ -44,16 +31,11 @@ function handleEnter() {
   );
 }
 
-// STEP 3
 function handleLeave() {
-  // STEP 6
-  console.log("LEAVE!!!");
   this.classList.remove("trigger-enter", "trigger-enter-active");
-  // STEP 8
   background.classList.remove("open");
 }
 
-// STEP 4
 triggers.forEach(trigger =>
   trigger.addEventListener("mouseenter", handleEnter)
 );
