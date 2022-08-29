d54ec3b9e41c3d4d656605acf597777ee127c3f4
Melissa Villegas <melissavillegas0898@gmail.com>
2022-08-28 20:26:26 -0400

    readme.md day-27

--- day-27/script.js
+++ day-27/script.js
@@ -1,44 +1,29 @@
-// STEP 1
 const slider = document.querySelector(".items");
 let isDown = false;
 let startX;
 let scrollLeft;
 
-// STEP 2
 slider.addEventListener("mousedown", e => {
   isDown = true;
-  // STEP 4
   slider.classList.add("active");
-  // STEP 7
-  // console.log(e.pageX);
   startX = e.pageX - slider.offsetLeft;
-  // STEP 9
   scrollLeft = slider.scrollLeft;
-  console.log(startX);
 });
+
 slider.addEventListener("mouseleave", () => {
   isDown = false;
-  // STEP 5
   slider.classList.remove("active");
 });
+
 slider.addEventListener("mouseup", () => {
   isDown = false;
-  // STEP 6
   slider.classList.remove("active");
 });
+
 slider.addEventListener("mousemove", e => {
-  // STEP 3
   if (!isDown) return;
-  // console.log(isDown);
-  // console.count(isDown);
-  // console.log("Do Work!");
-  // STEP 8
-  // console.log(startX);
-  // STEP 10
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
-  // console.log({ x, startX });
   const walk = (x - startX) * 3;
-  // console.log(walk);
   slider.scrollLeft = scrollLeft - walk;
 });
